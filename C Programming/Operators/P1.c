e145e02512906102e8d8e13aba3825ea65fbf6a3
parikhdev <itsyashparikh@gmail.com>
2026-04-15 07:37:23 +0530

    Initial commit

---
+++ C Programming/Operators/P1.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main()
+{
+    int a = 1, b = 2, c = 0, d;
+    d = --a || ++b && c++;
+    printf("%d%d%d%d", a, b, c, d);
+    return 0;
+}
---
+++ C Programming/Operators/P2.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main()
+{
+    int a = 0,b = 3,c;
+    c = a && b;
+    printf("%d", c);
+
+    return 0;
+}
